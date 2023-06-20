0d50784ba1ee5dc430a38187fc50789c22e35e49
KcirtapJhamez <134905897+KcirtapJhamez@users.noreply.github.com>
2023-06-20 09:14:32 +0800

    Add files via upload

---
+++ CC104 Stacks/Form1.Designer.vb
@@ -0,0 +1,414 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button2 = New Button()
+        Button1 = New Button()
+        txtnewdata = New TextBox()
+        txtstack0 = New TextBox()
+        txtstack1 = New TextBox()
+        txtstack2 = New TextBox()
+        txtstack3 = New TextBox()
+        txtstack4 = New TextBox()
+        Label1 = New Label()
+        Button3 = New Button()
+        Label2 = New Label()
+        Button4 = New Button()
+        Label3 = New Label()
+        Label4 = New Label()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Label5 = New Label()
+        txtqueue3 = New TextBox()
+        txtnextdata = New TextBox()
+        txtqueue2 = New TextBox()
+        txtqueue0 = New TextBox()
+        txtqueue4 = New TextBox()
+        Label6 = New Label()
+        txtqueue1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button2.Location = New Point(139, 203)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(166, 51)
+        Button2.TabIndex = 18
+        Button2.Text = "POP()"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button1.Location = New Point(139, 126)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(166, 51)
+        Button1.TabIndex = 17
+        Button1.Text = "PUSH()"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' txtnewdata
+        ' 
+        txtnewdata.BackColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
+        txtnewdata.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        txtnewdata.Location = New Point(335, 203)
+        txtnewdata.Name = "txtnewdata"
+        txtnewdata.Size = New Size(97, 26)
+        txtnewdata.TabIndex = 19
+        txtnewdata.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtstack0
+        ' 
+        txtstack0.BackColor = Color.White
+        txtstack0.BorderStyle = BorderStyle.FixedSingle
+        txtstack0.Enabled = False
+        txtstack0.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        txtstack0.Location = New Point(446, 296)
+        txtstack0.Multiline = True
+        txtstack0.Name = "txtstack0"
+        txtstack0.Size = New Size(125, 23)
+        txtstack0.TabIndex = 20
+        txtstack0.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtstack1
+        ' 
+        txtstack1.BackColor = Color.White
+        txtstack1.BorderStyle = BorderStyle.FixedSingle
+        txtstack1.Enabled = False
+        txtstack1.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        txtstack1.Location = New Point(446, 256)
+        txtstack1.Multiline = True
+        txtstack1.Name = "txtstack1"
+        txtstack1.Size = New Size(125, 23)
+        txtstack1.TabIndex = 21
+        txtstack1.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtstack2
+        ' 
+        txtstack2.BackColor = Color.White
+        txtstack2.BorderStyle = BorderStyle.FixedSingle
+        txtstack2.Enabled = False
+        txtstack2.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        txtstack2.Location = New Point(446, 217)
+        txtstack2.Multiline = True
+        txtstack2.Name = "txtstack2"
+        txtstack2.Size = New Size(125, 23)
+        txtstack2.TabIndex = 22
+        txtstack2.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtstack3
+        ' 
+        txtstack3.BackColor = Color.White
+        txtstack3.BorderStyle = BorderStyle.FixedSingle
+        txtstack3.Enabled = False
+        txtstack3.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        txtstack3.Location = New Point(446, 176)
+        txtstack3.Multiline = True
+        txtstack3.Name = "txtstack3"
+        txtstack3.Size = New Size(125, 23)
+        txtstack3.TabIndex = 23
+        txtstack3.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtstack4
+        ' 
+        txtstack4.BackColor = Color.White
+        txtstack4.BorderStyle = BorderStyle.FixedSingle
+        txtstack4.Enabled = False
+        txtstack4.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        txtstack4.Location = New Point(446, 140)
+        txtstack4.Multiline = True
+        txtstack4.Name = "txtstack4"
+        txtstack4.Size = New Size(125, 23)
+        txtstack4.TabIndex = 24
+        txtstack4.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label1.Location = New Point(446, 113)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 19)
+        Label1.TabIndex = 25
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button3.Location = New Point(600, 375)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(88, 32)
+        Button3.TabIndex = 26
+        Button3.Text = "Exit"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Snow
+        Label2.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label2.ForeColor = Color.Black
+        Label2.Location = New Point(345, 235)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(78, 18)
+        Label2.TabIndex = 27
+        Label2.Text = "Input Here"
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Button4
+        ' 
+        Button4.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button4.Location = New Point(139, 288)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(166, 51)
+        Button4.TabIndex = 28
+        Button4.Text = "Peek()"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Transparent
+        Label3.Font = New Font("Poor Richard", 20.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label3.ForeColor = Color.White
+        Label3.Location = New Point(139, 62)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(89, 31)
+        Label3.TabIndex = 29
+        Label3.Text = "Stacks;"
+        Label3.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = Color.Transparent
+        Label4.Font = New Font("Poor Richard", 20.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label4.ForeColor = Color.White
+        Label4.Location = New Point(673, 62)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(104, 31)
+        Label4.TabIndex = 30
+        Label4.Text = "Queues;"
+        Label4.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Button5
+        ' 
+        Button5.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button5.Location = New Point(634, 115)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(166, 51)
+        Button5.TabIndex = 31
+        Button5.Text = "Enqueue()"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button6.Location = New Point(978, 115)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(166, 51)
+        Button6.TabIndex = 40
+        Button6.Text = "Peek()"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button7.Location = New Point(806, 115)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(166, 51)
+        Button7.TabIndex = 32
+        Button7.Text = "Dequeue()"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.BackColor = Color.Snow
+        Label5.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label5.ForeColor = Color.Black
+        Label5.Location = New Point(855, 221)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(78, 18)
+        Label5.TabIndex = 39
+        Label5.Text = "Input Here"
+        Label5.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' txtqueue3
+        ' 
+        txtqueue3.BackColor = Color.White
+        txtqueue3.BorderStyle = BorderStyle.FixedSingle
+        txtqueue3.Enabled = False
+        txtqueue3.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        txtqueue3.Location = New Point(753, 257)
+        txtqueue3.Multiline = True
+        txtqueue3.Name = "txtqueue3"
+        txtqueue3.Size = New Size(87, 23)
+        txtqueue3.TabIndex = 37
+        txtqueue3.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtnextdata
+        ' 
+        txtnextdata.BackColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
+        txtnextdata.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        txtnextdata.Location = New Point(845, 192)
+        txtnextdata.Name = "txtnextdata"
+        txtnextdata.Size = New Size(97, 26)
+        txtnextdata.TabIndex = 33
+        txtnextdata.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtqueue2
+        ' 
+        txtqueue2.BackColor = Color.White
+        txtqueue2.BorderStyle = BorderStyle.FixedSingle
+        txtqueue2.Enabled = False
+        txtqueue2.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        txtqueue2.Location = New Point(846, 256)
+        txtqueue2.Multiline = True
+        txtqueue2.Name = "txtqueue2"
+        txtqueue2.Size = New Size(87, 23)
+        txtqueue2.TabIndex = 36
+        txtqueue2.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtqueue0
+        ' 
+        txtqueue0.BackColor = Color.White
+        txtqueue0.BorderStyle = BorderStyle.FixedSingle
+        txtqueue0.Enabled = False
+        txtqueue0.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        txtqueue0.Location = New Point(1032, 256)
+        txtqueue0.Multiline = True
+        txtqueue0.Name = "txtqueue0"
+        txtqueue0.Size = New Size(87, 23)
+        txtqueue0.TabIndex = 34
+        txtqueue0.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtqueue4
+        ' 
+        txtqueue4.BackColor = Color.White
+        txtqueue4.BorderStyle = BorderStyle.FixedSingle
+        txtqueue4.Enabled = False
+        txtqueue4.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        txtqueue4.Location = New Point(660, 257)
+        txtqueue4.Multiline = True
+        txtqueue4.Name = "txtqueue4"
+        txtqueue4.Size = New Size(87, 23)
+        txtqueue4.TabIndex = 38
+        txtqueue4.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.BackColor = Color.Transparent
+        Label6.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label6.ForeColor = Color.Wheat
+        Label6.Location = New Point(846, 298)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(0, 19)
+        Label6.TabIndex = 41
+        Label6.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' txtqueue1
+        ' 
+        txtqueue1.BackColor = Color.White
+        txtqueue1.BorderStyle = BorderStyle.FixedSingle
+        txtqueue1.Enabled = False
+        txtqueue1.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        txtqueue1.Location = New Point(939, 256)
+        txtqueue1.Multiline = True
+        txtqueue1.Name = "txtqueue1"
+        txtqueue1.Size = New Size(87, 23)
+        txtqueue1.TabIndex = 42
+        txtqueue1.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Wheat
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(1273, 419)
+        Controls.Add(txtqueue1)
+        Controls.Add(Label6)
+        Controls.Add(Button5)
+        Controls.Add(Button6)
+        Controls.Add(Button7)
+        Controls.Add(Label5)
+        Controls.Add(txtqueue3)
+        Controls.Add(txtnextdata)
+        Controls.Add(txtqueue2)
+        Controls.Add(txtqueue0)
+        Controls.Add(txtqueue4)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Button1)
+        Controls.Add(Button4)
+        Controls.Add(Button2)
+        Controls.Add(Button3)
+        Controls.Add(Label2)
+        Controls.Add(txtstack3)
+        Controls.Add(txtnewdata)
+        Controls.Add(txtstack2)
+        Controls.Add(txtstack0)
+        Controls.Add(txtstack4)
+        Controls.Add(Label1)
+        Controls.Add(txtstack1)
+        FormBorderStyle = FormBorderStyle.Fixed3D
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Stacks and Queues"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtnewdata As TextBox
+    Friend WithEvents txtstack0 As TextBox
+    Friend WithEvents txtstack1 As TextBox
+    Friend WithEvents txtstack2 As TextBox
+    Friend WithEvents txtstack3 As TextBox
+    Friend WithEvents txtstack4 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtqueue3 As TextBox
+    Friend WithEvents txtnextdata As TextBox
+    Friend WithEvents txtqueue2 As TextBox
+    Friend WithEvents txtqueue0 As TextBox
+    Friend WithEvents txtqueue4 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtqueue1 As TextBox
+End Class
---
+++ CC104 Stacks/Form1.vb
@@ -0,0 +1,122 @@
+﻿Imports System.Drawing.Text
+Imports System.IO
+Imports System.Net.Security
+
+Public Class Form1
+    'Stack
+    Dim data(4) As String
+    Const maxsize As Integer = 4
+    Dim Stack_Top As Integer = -1
+
+    'Queue
+    Dim d(4) As String
+    Const max As Integer = 4
+    Dim Queue_Front As Integer = 0
+    Dim Queue_Rear As Integer = -1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'PUSH
+        Me.txtnewdata.Focus()
+        Dim out As String = "Stack is Full / Overflow" & vbCrLf
+        If Stack_Top = maxsize Then
+            Me.Label1.ForeColor = Color.Red
+            Me.Label1.Text = "Stack is Full / Overflow"
+            MessageBox.Show(out, "Error***")
+            Me.Close()
+        Else
+            Stack_Top = Stack_Top + 1
+            data(Stack_Top) = Me.txtnewdata.Text
+            Me.Controls("txtstack" & Stack_Top).Text = Me.txtnewdata.Text
+            Me.txtnewdata.Text = ""
+            Me.Label1.ForeColor = Color.Green
+            Me.Label1.Text = "Stack Inserted : " + data(Stack_Top)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.txtnewdata.Focus()
+        Dim out As String = "Stack is Empty!" & vbCrLf
+        If Stack_Top = -1 Then
+            Me.Label1.ForeColor = Color.Red
+            Me.Label1.Text = "Stack is Empty!"
+            MessageBox.Show(out, "Error***")
+            Me.Close()
+        Else
+            Me.Label1.ForeColor = Color.Red
+            Me.Label1.Text = "Item Popped : " + data(Stack_Top)
+            Me.Controls("txtstack" & Stack_Top).Text = ""
+            Stack_Top = Stack_Top - 1
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ' stack peek()
+        Me.txtnewdata.Focus()
+        Dim out As String = "Stack is Empty!" & vbCrLf
+
+        If Stack_Top = -1 Then
+            Me.Label1.ForeColor = Color.Red
+            Me.Label1.Text = "Stack is Empty!"
+            MessageBox.Show(out, "Error***")
+            Me.Close()
+        Else
+            MessageBox.Show("The Peek is : " + data(Stack_Top), "Stack Peek()")
+        End If
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        'Enqueue()
+        Me.txtnextdata.Focus()
+        Dim pat As String = "Queue is Full / Overflow" & vbCrLf
+        If Queue_Rear = max Then
+            Me.Label6.ForeColor = Color.Red
+            Me.Label6.Text = "Queue is Full / Overflow"
+            MessageBox.Show(pat, "Error***")
+            Me.Close()
+        Else
+            Queue_Rear = Queue_Rear + 1
+            d(Queue_Rear) = Me.txtnextdata.Text
+            Me.Controls("txtqueue" & Queue_Rear).Text = Me.txtnextdata.Text
+            Me.txtnextdata.Text = ""
+            Me.Label6.ForeColor = Color.Green
+            Me.Label6.Text = "Enqueued Item : " + d(Queue_Rear)
+        End If
+    End Sub
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Me.txtnextdata.Focus()
+        Dim pat As String = "Queue is Empty" & vbCrLf
+
+        If Queue_Front > Queue_Rear Then
+            Me.Label6.ForeColor = Color.Red
+            Me.Label6.Text = "Queue is Empty!"
+            MessageBox.Show(pat, "Error***")
+            Me.Close()
+        Else
+            Me.Label6.ForeColor = Color.Red
+            Me.Label6.Text = "Dequeued Item: " + d(Queue_Front)
+            Me.Controls("txtqueue" & Queue_Front).Text = ""
+
+            For i As Integer = Queue_Front + 1 To Queue_Rear
+                Me.Controls("txtqueue" & i - 1).Text = Me.Controls("txtqueue" & i).Text
+                d(i - 1) = d(i)
+            Next
+            Me.Controls("txtqueue" & Queue_Rear).Text = ""
+            Queue_Rear = Queue_Rear - 1
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Me.txtnextdata.Focus()
+        Dim pat As String = "Queue is Empty" & vbCrLf
+        If Queue_Front > Queue_Rear Then
+            Me.Label6.ForeColor = Color.Red
+            Me.Label6.Text = "Queue is Empty!"
+            MessageBox.Show(pat, "Error***")
+            Me.Close()
+        Else
+            MessageBox.Show("The Peek is : " + d(Queue_Front), "Queue Peek()")
+        End If
+    End Sub
+End Class
